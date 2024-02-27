326d411ca0c8df3240f28f84fd06f77d1f22036e
Ahmad Raza <ahmadraza20082003@gmail.com>
2024-02-27 11:09:15 +0530

    Added Resume

--- src/components/footercomp.jsx
+++ src/components/footercomp.jsx
@@ -32,7 +32,7 @@ const Footercomp = () => {
             <div className="social">
                 <a href="https://www.instagram.com/roneeey_09/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
                 <a href="https://github.com/ahmadraza-09" target="_blank"><i className="fa-brands fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/ahmad-raza-b4331727a/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
+                <a href="https://www.linkedin.com/in/ahmadraza09/" target="_blank"><i className="fa-brands fa-linkedin-in"></i></a>
                 <a href="https://www.youtube.com/channel/UC55vehqjvMVqg_HTTJr2HGQ" target="_blank"><i className="fa-brands fa-youtube"></i></a>
             </div>
         </ul>
--- src/components/sociallinkscomp.jsx
+++ src/components/sociallinkscomp.jsx
@@ -5,7 +5,7 @@ const Sociallinkscomp = () => {
         <div className="social-media-links">
             <a href="https://www.instagram.com/roneeey_09/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
             <a href="https://github.com/ahmadraza-09" target="_blank"><i class="fa-brands fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/ahmad-raza-b4331727a/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://www.linkedin.com/in/ahmadraza09/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
             <a href="https://www.youtube.com/channel/UC55vehqjvMVqg_HTTJr2HGQ" target="_blank"><i class="fa-brands fa-youtube"></i></a>
         </div>
     </>
